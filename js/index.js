2e795df37580b691b28b913965860fa130cb65be
Gabriel Borgonovo <gabrielalejandroborgonovo@gmail.com>
2023-05-19 17:13:58 -0300

    burguer buttom

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-console.log('conectado')
+console.log('conectado');
+
+const $btn = document.querySelector(".btn-burguer");
+const $ulRealizadas = document.querySelector('.ul-realizadas');
+console.log($ulRealizadas)
+
+document.addEventListener('click', (e) => {
+    if(e.target === $btn){
+        $ulRealizadas.classList.toggle('esconder');
+    }
+})
