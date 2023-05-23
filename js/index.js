396db0bcbfe026c774ab24ad7b246e25753d741a
Gabriel Borgonovo <gabrielalejandroborgonovo@gmail.com>
2023-05-23 09:21:52 -0300

    agenda actualizada una vez más

--- js/index.js
+++ js/index.js
@@ -4,14 +4,12 @@ const mostrar = () => {
     const $btn = document.querySelector(".btn-burguer");
     const $ulRealizadas = document.querySelector('.ul-realizadas');
 
-    document.addEventListener('click', (e) => {
-        if(e.target === $btn){
-            $ulRealizadas.classList.toggle('mostrar');
-        }
+    $btn.addEventListener('click', () => {
+        $ulRealizadas.classList.toggle('mostrar');
     });
 }
 
-document.addEventListener('DOMContentLoaded', (e) => {
+window.addEventListener('load', (event) => {
     mostrar();
-})
+});
 
